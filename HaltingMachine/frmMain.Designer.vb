848889de76b31ba40eb50d94c5f96bb5fa7e85c2
William Mollenkopf III <biri@biri.me>
2016-08-18 10:09:29 -0400

    First iteration of the Halting Machine. This program is intended to run another program and halt the program after a period of time if the program does not complete it's execution within that time frame.

---
+++ HaltingMachine/frmMain.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtFile = New System.Windows.Forms.TextBox()
+        Me.prgbarStatus = New System.Windows.Forms.ProgressBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.timerTimeout = New System.Windows.Forms.Timer(Me.components)
+        Me.lblTimerDisplay = New System.Windows.Forms.Label()
+        Me.lblPercent = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtHours = New System.Windows.Forms.TextBox()
+        Me.txtMinutes = New System.Windows.Forms.TextBox()
+        Me.txtSeconds = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnRun
+        '
+        Me.btnRun.Location = New System.Drawing.Point(577, 53)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(75, 23)
+        Me.btnRun.TabIndex = 0
+        Me.btnRun.Text = "Run It"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Which program?"
+        '
+        'txtFile
+        '
+        Me.txtFile.Location = New System.Drawing.Point(112, 53)
+        Me.txtFile.Name = "txtFile"
+        Me.txtFile.Size = New System.Drawing.Size(450, 20)
+        Me.txtFile.TabIndex = 2
+        Me.txtFile.Text = "Pilot.exe"
+        '
+        'prgbarStatus
+        '
+        Me.prgbarStatus.Location = New System.Drawing.Point(242, 107)
+        Me.prgbarStatus.Name = "prgbarStatus"
+        Me.prgbarStatus.Size = New System.Drawing.Size(395, 23)
+        Me.prgbarStatus.Step = 1
+        Me.prgbarStatus.TabIndex = 3
+        Me.prgbarStatus.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Meiryo UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(237, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(200, 30)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Halting Machine"
+        '
+        'timerTimeout
+        '
+        '
+        'lblTimerDisplay
+        '
+        Me.lblTimerDisplay.AutoSize = True
+        Me.lblTimerDisplay.Location = New System.Drawing.Point(317, 147)
+        Me.lblTimerDisplay.Name = "lblTimerDisplay"
+        Me.lblTimerDisplay.Size = New System.Drawing.Size(0, 13)
+        Me.lblTimerDisplay.TabIndex = 5
+        Me.lblTimerDisplay.Visible = False
+        '
+        'lblPercent
+        '
+        Me.lblPercent.AutoSize = True
+        Me.lblPercent.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPercent.Location = New System.Drawing.Point(435, 110)
+        Me.lblPercent.Name = "lblPercent"
+        Me.lblPercent.Size = New System.Drawing.Size(14, 13)
+        Me.lblPercent.TabIndex = 6
+        Me.lblPercent.Text = "0"
+        Me.lblPercent.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(202, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "For How Long? (Hours:Minutes:Seconds)"
+        '
+        'txtHours
+        '
+        Me.txtHours.Location = New System.Drawing.Point(77, 107)
+        Me.txtHours.MaxLength = 3
+        Me.txtHours.Name = "txtHours"
+        Me.txtHours.Size = New System.Drawing.Size(42, 20)
+        Me.txtHours.TabIndex = 8
+        Me.txtHours.Text = "0"
+        Me.txtHours.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtMinutes
+        '
+        Me.txtMinutes.Location = New System.Drawing.Point(125, 107)
+        Me.txtMinutes.MaxLength = 3
+        Me.txtMinutes.Name = "txtMinutes"
+        Me.txtMinutes.Size = New System.Drawing.Size(42, 20)
+        Me.txtMinutes.TabIndex = 9
+        Me.txtMinutes.Text = "10"
+        Me.txtMinutes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtSeconds
+        '
+        Me.txtSeconds.Location = New System.Drawing.Point(173, 107)
+        Me.txtSeconds.MaxLength = 3
+        Me.txtSeconds.Name = "txtSeconds"
+        Me.txtSeconds.Size = New System.Drawing.Size(42, 20)
+        Me.txtSeconds.TabIndex = 10
+        Me.txtSeconds.Text = "0"
+        Me.txtSeconds.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'frmPilot
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(678, 175)
+        Me.Controls.Add(Me.txtSeconds)
+        Me.Controls.Add(Me.txtMinutes)
+        Me.Controls.Add(Me.txtHours)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblPercent)
+        Me.Controls.Add(Me.lblTimerDisplay)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.prgbarStatus)
+        Me.Controls.Add(Me.txtFile)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnRun)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "frmPilot"
+        Me.Text = "Pilot"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnRun As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtFile As System.Windows.Forms.TextBox
+    Friend WithEvents prgbarStatus As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents timerTimeout As System.Windows.Forms.Timer
+    Friend WithEvents lblTimerDisplay As System.Windows.Forms.Label
+    Friend WithEvents lblPercent As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtHours As System.Windows.Forms.TextBox
+    Friend WithEvents txtMinutes As System.Windows.Forms.TextBox
+    Friend WithEvents txtSeconds As System.Windows.Forms.TextBox
+
+End Class
---
+++ HaltingMachine/frmMain.vb
@@ -0,0 +1,95 @@
+﻿Public Class frmMain
+
+    Private TimeoutTime As Integer = 1 * 60 * 100 ' Default: 10 minutes, 60 seconds per min, 100 milliseconds per second ? (thought it was 1000 but... vb.net says otherwise?)
+    Private ProcessProperties As New ProcessStartInfo
+    Private myProcess As Process
+    Private percentCounter As Double = 0
+    Private millisecondCounter As Double = 0
+
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+        ToggleVisabilityAccessibility() ' assumed initalized by form design definition
+
+        ' Calculate the time
+        TimeoutTime = (txtHours.Text * 60 * 60 * 100) + (txtMinutes.Text * 60 * 100) + (txtSeconds.Text * 100)
+        timerTimeout.Start() ' Start the timer
+
+        ' Assign the process the file to use and execute
+        Try
+            ProcessProperties.FileName = txtFile.Text
+            myProcess = Process.Start(ProcessProperties)
+        Catch ex As Exception
+            timerTimeout.Stop()
+            MsgBox("An error has occured. Perhaps the filename and path are bad?")
+            ToggleVisabilityAccessibility() ' assumed initalized by form design definition
+        End Try
+
+    End Sub
+
+    Private Sub ToggleVisabilityAccessibility()
+        txtFile.Enabled = Not txtFile.Enabled
+        prgbarStatus.Visible = Not prgbarStatus.Visible
+        btnRun.Enabled = Not btnRun.Enabled
+        lblTimerDisplay.Visible = Not lblTimerDisplay.Visible
+        lblPercent.Visible = Not lblPercent.Visible
+
+        txtHours.Enabled = Not txtHours.Enabled
+        txtMinutes.Enabled = Not txtMinutes.Enabled
+        txtSeconds.Enabled = Not txtSeconds.Enabled
+    End Sub
+    Private Sub timerTimeout_Tick(sender As Object, e As EventArgs) Handles timerTimeout.Tick
+        If prgbarStatus.Value < 100 Then
+            millisecondCounter += 1
+            percentCounter += 100 / TimeoutTime
+            prgbarStatus.Value = percentCounter
+            lblTimerDisplay.Text = getMinSecText(millisecondCounter) + " / " + getMinSecText(TimeoutTime / 10)
+            lblPercent.Text = (Math.Round(percentCounter, 2)).ToString() + " / 100% "
+
+            prgbarStatus.Refresh()
+
+            If myProcess.HasExited Then
+                ' All done
+                timerTimeout.Stop()
+                ToggleVisabilityAccessibility() ' make ready again for use
+            End If
+        Else
+            If Not myProcess.HasExited Then
+                myProcess.Kill()
+            End If
+            timerTimeout.Stop()
+
+            prgbarStatus.Value = 100
+            ToggleVisabilityAccessibility() ' make ready again for use
+
+        End If
+    End Sub
+
+    Private Function getMinSecText(milliseconds As Integer)
+        ' Hours:Minutes:Seconds
+        Return Math.Floor((milliseconds / 10 / 60 / 60)).ToString("00") + ":" + Math.Floor((milliseconds / 10 / 60)).ToString("00") + ":" + Math.Round(((milliseconds / 10) Mod 60), 2).ToString("00")
+    End Function
+
+    Private Sub time_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtHours.KeyPress, txtMinutes.KeyPress
+        'Source: http://stackoverflow.com/questions/9969824/vb-net-need-text-box-to-only-accept-numbers
+        '97 - 122 = Ascii codes for simple letters
+        '65 - 90  = Ascii codes for capital letters
+        '48 - 57  = Ascii codes for numbers
+
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub frmPilot_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+        ' Display a MsgBox asking the user to save changes or abort.
+        If Not IsNothing(myProcess) AndAlso Not myProcess.HasExited Then
+            If MessageBox.Show("Are you sure? Closing this program will also attempt to force close """ + txtFile.Text + """ also.", "Are you sure?", MessageBoxButtons.YesNo) = DialogResult.No Then
+                ' Cancel the Closing event from closing the form.
+                e.Cancel = True
+            Else
+                myProcess.Kill() ' force this to close
+            End If
+        End If
+    End Sub
+End Class
